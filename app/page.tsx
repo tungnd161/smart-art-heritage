0be928df87985dadc8cd943c27e370718eec5824
Tung Nguyen <tungnd16186@gmail.com>
2026-09-14 15:01:06 +0700

    force dynamic rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { createClient } from "@supabase/supabase-js"
 import Link from "next/link"
 
+export const dynamic = 'force-dynamic'
+
 export default async function Home() {
   const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
